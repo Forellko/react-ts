1bdd013e84a53b27a3e6e46e3018d350c1ff0a5b
forellko <dragonforce97q@gmail.com>
2022-12-03 12:22:19 +0300

    Get products from fake api with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import Product from './components/Product';
+import { products } from './data/products';
+import { IProduct } from './models';
 
 function App() {
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>(
+      'https://fakestoreapi.com/products'
+    );
+    console.log(response);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product />
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
 }
