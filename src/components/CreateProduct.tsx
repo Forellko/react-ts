a6ec47e2244af6ff5bb7cae8c96fd0c8bb66a1c8
forellko <dragonforce97q@gmail.com>
2022-12-03 13:38:50 +0300

    Add onChange handler with types

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function CreateProduct() {
-  const submit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const [value, setValue] = useState('');
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
   };
 
   return (
-    <form onSubmit={submit}>
+    <form onSubmit={submitHandler}>
       <input
         type="text"
         className="border py-2 px-4 mb-2 w-full outline-none"
         placeholder="Enter product title..."
+        value={value}
+        onChange={changeHandler}
       />
 
       <button
