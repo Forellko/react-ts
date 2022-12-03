9977aea3d16f36f1055f6a483dd1838ca924a3f8
Forellko <dragonforce97q@gmail.com>
2022-12-03 20:00:06 +0300

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import { useContext, useState } from 'react';
-import CreateProduct from './components/CreateProduct';
-import ErrorMessage from './components/ErrorMessage';
-import Loader from './components/Loader';
-import Modal from './components/Modal';
-import Product from './components/Product';
-import { ModalContext } from './context/ModalContext';
-import { useProducts } from './hooks/products.hook';
-import { IProduct } from './models';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import AboutPage from './Pages/AboutPage';
+import ProductPage from './Pages/ProductPage';
 
 function App() {
-  const { products, loading, error, addProduct } = useProducts();
-  const { modal, open, close } = useContext(ModalContext);
-
-  const onCreate = (product: IProduct) => {
-    close();
-    addProduct(product);
-  };
-
   return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-      {modal && (
-        <Modal title="Create new product" onClose={close}>
-          <CreateProduct onCreate={onCreate} />
-        </Modal>
-      )}
-
-      <button
-        className="fixed bottom-5 right-5  bg-green-700 text-white text-2xl px-5 py-2 rounded"
-        onClick={open}
-      >
-        Add
-      </button>
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<ProductPage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/ProductPage.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useContext, useState } from 'react';
+import CreateProduct from '../components/CreateProduct';
+import ErrorMessage from '../components/ErrorMessage';
+import Loader from '../components/Loader';
+import Modal from '../components/Modal';
+import Product from '../components/Product';
+import { ModalContext } from '../context/ModalContext';
+import { useProducts } from '../hooks/products.hook';
+import { IProduct } from '../models';
+
+export default function ProductPage() {
+  const { products, loading, error, addProduct } = useProducts();
+  const { modal, open, close } = useContext(ModalContext);
+
+  const onCreate = (product: IProduct) => {
+    close();
+    addProduct(product);
+  };
+
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+      {modal && (
+        <Modal title="Create new product" onClose={close}>
+          <CreateProduct onCreate={onCreate} />
+        </Modal>
+      )}
+
+      <button
+        className="fixed bottom-5 right-5  bg-green-700 text-white text-2xl px-5 py-2 rounded"
+        onClick={open}
+      >
+        Add
+      </button>
+    </div>
+  );
+}
