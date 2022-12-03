10b19986f93c2809454ab7cccb00eabb3adb29ca
forellko <dragonforce97q@gmail.com>
2022-12-03 13:02:42 +0300

    Put out Loading and ErrorMessage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import ErrorMessage from './components/ErrorMessage';
+import Loader from './components/Loader';
 import Product from './components/Product';
 import { useProducts } from './hooks/products.hook';
-import { IProduct } from './models';
 
 function App() {
-  const {products, loading, error} = useProducts()
+  const { products, loading, error } = useProducts();
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <p className="text-center">Loading...</p>}
-      {error && <p className="text-center text-red-700">Error</p>}
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
