a0d69378aaf152838c9c6f61c300551f00597702
forellko <dragonforce97q@gmail.com>
2022-12-03 12:11:54 +0300

    Add toggle button to Product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { IProduct } from '../models';
+interface ProductProps {
+  product: IProduct;
+}
 
-export default function Product() {
+export default function Product({ product }: ProductProps) {
+  const [isOpenDescription, setIsOpenDescription] = useState(false);
+  const toggleDesciption = () => setIsOpenDescription((prev) => !prev);
+
   return (
-    <div>Product</div>
-  )
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img
+        src={product.image}
+        className="w-1/6 my-[25px]"
+        alt={product.title}
+      />
+      <p>{product.title}</p>
+      <span className="font-bold my-[25px]">{product.price}</span>
+      <button
+        onClick={toggleDesciption}
+        className={`h-[50px] w-[200px] my-[25px] ${
+          isOpenDescription ? 'bg-slate-200' : 'bg-slate-300'
+        } `}
+      >
+        {!isOpenDescription && 'Show details'}
+        {isOpenDescription && 'Hide'}
+      </button>
+      {isOpenDescription && <p>{product.description}</p>}
+    </div>
+  );
 }
